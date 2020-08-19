82692fe693d22b57db4cacdc3ac7660a14621e4c
Abram Simon <abram@abramsimon.com>
2020-08-19 12:27:50 -0400

    ono web: added axios call to server to get list of ingredients

--- web/src/components/ingredient-list/ingredient-list.tsx
+++ web/src/components/ingredient-list/ingredient-list.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { initialIngredients, Ingredient } from '../../models/ingredient';
 import './ingredient-list.css';
 
-const IngredientList = () => (
-  <div>
-    <h2>Ingredients</h2>
-  </div>
-);
+const IngredientList = () => {
+  const [ingredients, setIngredients] = useState(initialIngredients);
+
+  useEffect(() => {
+    axios('http://localhost:8080/api/ingredients').then((results) => {
+      // map the results
+      let mappedResults: Ingredient[] = [];
+      for (let result of results.data) {
+        mappedResults.push({
+          name: result.name,
+          image: result.image,
+          holdingTemperature: result.holding_temperature,
+          hopper: '',
+          dispenseGrams: 0,
+          usedBy: '',
+          demandKg: 0,
+          densityKgM3: 0,
+          capacityL: 0,
+        });
+      }
+
+      setIngredients(mappedResults);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Ingredients</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default IngredientList;
--- web/src/models/ingredient.ts
+++ web/src/models/ingredient.ts
@@ -1,4 +1,4 @@
-export default interface Ingredient {
+export interface Ingredient {
   name: string;
   image: string;
   holdingTemperature: number;
@@ -9,3 +9,30 @@ export default interface Ingredient {
   densityKgM3: number;
   capacityL: number;
 }
+
+export const initialIngredients: Ingredient[] = [
+  {
+    name: 'Roasted chicken',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3HWyvSSc2uPJ1PxMEKcSSkidwE5H6YyjVFNjEca9vQVO54TC9&s',
+    holdingTemperature: 140,
+    hopper: 'auger feeder',
+    dispenseGrams: 80,
+    usedBy: 'Harvest Bowl',
+    demandKg: 27.2,
+    densityKgM3: 1121,
+    capacityL: 24.3,
+  },
+  {
+    name: 'sweet potatoes',
+    image:
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/180802-delish-9x13-bookazine-00360-1533324368.jpg',
+    holdingTemperature: 140,
+    hopper: 'vibratory pan',
+    dispenseGrams: 39,
+    usedBy: 'Harvest Bowl',
+    demandKg: 7.1175,
+    densityKgM3: 270,
+    capacityL: 26.4,
+  },
+];
